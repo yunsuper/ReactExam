1fd7d0adb3203bfb14ff89daac103327463e0123
yunsuper <yunsuper80@gmaile.com>
2025-11-11 13:25:36 +0900

    ReactExam

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+ {/* 
+  작성자: lch
+  작성일 : 2030. 5.30.
+  내용: 기능에 대한 내용
+ */}
+
 function App() {
+  let name = "리액트";
+  const style = {
+    backgroundColor : 'black',
+    color : 'white',
+    fontSize : '48px',
+    fontWeight: 'bold',
+    padding : '20px'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="style">
+          <h1 className="test">
+              Hello,
+              {name === "리액트" ? <h1>YES</h1> : null}!!
+          </h1>
+      {/* <p>반갑습니다.</p>  */}
+      </div>
+  );  
 }
 
 export default App;
